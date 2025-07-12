6ad7a7a8a6a4f9b9a801183015177d07dcca6ad6
levi9111 <captainlevi9111@gmail.com>
2025-07-12 16:15:07 +0600

    Reviews API created

--- src/redux/api/reviewsApi.ts
+++ src/redux/api/reviewsApi.ts
@@ -1,5 +1,44 @@
 import { baseApi } from './baseApi';
 
 export const reviewsApi = baseApi.injectEndpoints({
-  endpoints: (builder) => ({}),
+  endpoints: (builder) => ({
+    createReview: builder.mutation({
+      query: (formdata) => ({
+        url: '/reviews/create-review',
+        method: 'POST',
+        body: formdata,
+      }),
+    }),
+
+    updateReview: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/reviews/update-review/${id}`,
+        method: 'PATCH',
+        body: data,
+      }),
+    }),
+
+    getAllReviews: builder.query({
+      query: () => '/reviews',
+    }),
+
+    getSingleReview: builder.query({
+      query: (id: string) => `/reviews/${id}`,
+    }),
+
+    deleteReview: builder.mutation({
+      query: (id: string) => ({
+        url: `/reviews/delete-review/${id}`,
+        method: 'DELETE',
+      }),
+    }),
+  }),
 });
+
+export const {
+  useCreateReviewMutation,
+  useUpdateReviewMutation,
+  useGetAllReviewsQuery,
+  useGetSingleReviewQuery,
+  useDeleteReviewMutation,
+} = reviewsApi;
